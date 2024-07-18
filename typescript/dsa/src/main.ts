cabfab983f0e3f84f00737297cbb987e0fe6c067
Avi Jassra <avijassra@gmail.com>
2024-07-18 16:54:15 -0400

    working main file

--- typescript/dsa/src/main.ts
+++ typescript/dsa/src/main.ts
@@ -13,28 +13,50 @@ export class Main {
         const rl = readline.createInterface({
             input: process.stdin,
             output: process.stdout
-        });
+        })
 
-        let sortType!: string;
-        rl.question('Enter the sorting type (bubble/selection/insertion/merge/quick): ', (answer) => {
-            sortType = answer.toLowerCase();
-            rl.close();
-        });
+        let sortType!: string
+        let sortDir!: string
 
-        let sortDirection!: string;
-        rl.question('Enter the sorting direction (asc/desc): ', (answer) => {
-            sortDirection = answer.toLowerCase();
-            rl.close();
-        });
+        const question1 = () => {
+            return new Promise<void>((resolve, reject) => {
+                rl.question('1. Enter the sorting type (bubble/selection/insertion/merge/quick): ', (answer) => {
+                    sortType = answer.toLowerCase();
+                    resolve()
+                })
+            })
+        }
 
+        const question2 = () => {
+            return new Promise<void>((resolve, reject) => {
+                rl.question('2. Enter the sorting direction (asc/desc): ', (answer) => {
+                    sortDir = answer.toLowerCase();
+                    resolve()
+                })
+            })
+        }
+
+        const questions = async () => {
+            await question1()
+            await question2()
+
+            this.runSorting(sortType, sortDir);
+
+            rl.close()
+        }
+
+        questions();
+    }
+
+    runSorting(sortType: string, sortDirection: string): void {
         const srcArr = [34, 22, 54, 65, 21, 23, 43, 32, 12, 11, 2, 5, 3, 12, 62, 43, 54, 65, 76, 87, 98, 89, 78, 67, 56, 45, 34, 23, 12, 11, 10, 9, 7, 1, 4, 8, 2];
         console.log(`Before sorting: ${srcArr}`);
 
         const sorter: ISorting = this.resolveSortType(sortType || '');
         let sortedArr!: number[];
 
-        if (sortDirection === 'desc') {
-            sortedArr= sorter.sort(srcArr);
+        if (sortDirection === 'asc') {
+            sortedArr = sorter.sort(srcArr);
         } else {
             sortedArr = sorter.reverseSort(srcArr);
         }
